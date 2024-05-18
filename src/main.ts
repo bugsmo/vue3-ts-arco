dc265ef56c5e321f613fe49d28e2d5659312ba0b
bugsmo <kalandramo@gmail.com>
2024-05-18 20:45:28 +0800

    :boom: feat: vite配置

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import App from './App.vue'
 import 'vue-global-api'
 import router from './router'
 
+// 自定义指令
+import pinia from '@/stores'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
